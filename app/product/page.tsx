5ef8c4f1058b79e41239a978ebde1bd1f106b465
Tofig Valiyev <tofig.valiyev1993@gmail.com>
2026-07-23 23:06:11 +0200

    first commit

---
+++ app/product/page.tsx
@@ -0,0 +1,7 @@
+const ProductPage = () => {
+  return (
+    <>This page will return mapped products, then will be inserted into Landing page where products will displayed</>
+  );
+};
+
+export default ProductPage;
